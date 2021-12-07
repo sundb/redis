8a1f9beaf1c1c3bf7555316ee8b8993e2f61f3d0
Binbin <binloveplay1314@qq.com>
2021-12-07 18:02:58 +0800

    Fix timing issue in logging.tcl with FreeBSD (#9910)
    A test failure was reported in Daily CI.
    `Crash report generated on SIGABRT` with FreeBSD.
    
    ```
    *** [err]: Crash report generated on SIGABRT in tests/integration/logging.tcl
    Expected [string match *crashed by signal* ### Starting...(logs) in tests/integration/logging.tcl]
    ```
    
    It look like `tail -1000` was executed too early, before it
    printed out all the crash logs. We can give it a few more
    chances by using `wait_for_log_messages`.
    
    Other changes:
    1. In `Server is able to generate a stack trace on selected systems`,
    use `wait_for_log_messages`to reduce the lines of code. And if it
    fails, there are more detailed logs that can be printed.
    
    2. In `Crash report generated on DEBUG SEGFAULT`, we also use
    `wait_for_log_messages` to avoid possible timing issues.

--- tests/integration/logging.tcl
+++ tests/integration/logging.tcl
@@ -19,18 +19,8 @@ if {$system_supported} {
             r config set watchdog-period 200
             r debug sleep 1
             set pattern "*debugCommand*"
-            set retry 10
-            while {$retry} {
-                set result [exec tail -100 < [srv 0 stdout]]
-                if {[string match $pattern $result]} {
-                    break
-                }
-                incr retry -1
-                after 1000
-            }
-            if {$retry == 0} {
-                error "assertion:expected stack trace not found into log file"
-            }
+            set res [wait_for_log_messages 0 \"$pattern\" 0 100 100]
+            if {$::verbose} { puts $res }
         }
     }
 
@@ -41,9 +31,8 @@ if {$system_supported} {
             test "Crash report generated on SIGABRT" {
                 set pid [s process_id]
                 exec kill -SIGABRT $pid
-                set pattern "*STACK TRACE*"
-                set result [exec tail -1000 < [srv 0 stdout]]
-                assert {[string match $pattern $result]}
+                set res [wait_for_log_messages 0 {"*STACK TRACE*"} 0 50 100]
+                if {$::verbose} { puts $res }
             }
         }
     }
