ba9198f2ce39b7fd7a1769ecab5303468d65b89f
debing.sun <debing.sun@redis.com>
2024-03-26 15:48:18 +0800

    add signal tag for some tests

--- tests/unit/moduleapi/crash.tcl
+++ tests/unit/moduleapi/crash.tcl
@@ -33,31 +33,31 @@ if {!$::valgrind} {
 
     start_server {tags {"modules"}} {
         r module load $testmodule segfault
-        # test {Test module crash when info crashes with a segfault} {
-        #     catch {r 0 info infocrash}
-        #     set res [wait_for_log_messages 0 {"*=== REDIS BUG REPORT START: Cut & paste starting from here ===*"} 0 10 1000]
-        #     set loglines [lindex $res 1]
+        test {Test module crash when info crashes with a segfault} {
+            catch {r 0 info infocrash}
+            set res [wait_for_log_messages 0 {"*=== REDIS BUG REPORT START: Cut & paste starting from here ===*"} 0 10 1000]
+            set loglines [lindex $res 1]
 
-        #     if {$backtrace_supported} {
-        #         set res [wait_for_log_messages 0 {"*Crashed running the instruction at*"} $loglines 10 1000]
-        #         set loglines [lindex $res 1]
+            if {$backtrace_supported} {
+                set res [wait_for_log_messages 0 {"*Crashed running the instruction at*"} $loglines 10 1000]
+                set loglines [lindex $res 1]
 
-        #         set res [wait_for_log_messages 0 {"*Crashed running signal handler. Providing reduced version of recursive crash report*"} $loglines 10 1000]
-        #         set loglines [lindex $res 1]
-        #         set res [wait_for_log_messages 0 {"*Crashed running the instruction at*"} $loglines 10 1000]
-        #         set loglines [lindex $res 1]
-        #     }
+                set res [wait_for_log_messages 0 {"*Crashed running signal handler. Providing reduced version of recursive crash report*"} $loglines 10 1000]
+                set loglines [lindex $res 1]
+                set res [wait_for_log_messages 0 {"*Crashed running the instruction at*"} $loglines 10 1000]
+                set loglines [lindex $res 1]
+            }
 
-        #     wait_for_log_messages 0 {"*=== REDIS BUG REPORT END. Make sure to include from START to END. ===*"} $loglines 10 1000
-        #     assert_equal 1 [count_log_message 0 "=== REDIS BUG REPORT END. Make sure to include from START to END. ==="]
-        #     assert_equal 1 [count_log_message 0 "Crashed running signal handler. Providing reduced version of recursive crash report"]
-        #     if {$backtrace_supported} {
-        #         assert_equal 2 [count_log_message 0 "Crashed running the instruction at"]
-        #         # Make sure the crash trace is printed twice. There will be 3 instances of 
-        #         # modulesCollectInfo, 1 in the first stack trace and 2 in the second.
-        #         assert_equal 3 [count_log_message 0 "modulesCollectInfo"]
-        #     }
-        #     assert_equal 1 [count_log_message 0 "=== REDIS BUG REPORT START: Cut & paste starting from here ==="]
-        # }
+            wait_for_log_messages 0 {"*=== REDIS BUG REPORT END. Make sure to include from START to END. ===*"} $loglines 10 1000
+            assert_equal 1 [count_log_message 0 "=== REDIS BUG REPORT END. Make sure to include from START to END. ==="]
+            assert_equal 1 [count_log_message 0 "Crashed running signal handler. Providing reduced version of recursive crash report"]
+            if {$backtrace_supported} {
+                assert_equal 2 [count_log_message 0 "Crashed running the instruction at"]
+                # Make sure the crash trace is printed twice. There will be 3 instances of 
+                # modulesCollectInfo, 1 in the first stack trace and 2 in the second.
+                assert_equal 3 [count_log_message 0 "modulesCollectInfo"]
+            }
+            assert_equal 1 [count_log_message 0 "=== REDIS BUG REPORT START: Cut & paste starting from here ==="]
+        } {} {signal}
     }
 }
