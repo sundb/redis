a7cd349fc26f8659e1e6441588b34a8e58b1f8f3
debing.sun <debing.sun@redis.com>
2024-11-14 15:13:15 +0800

    uncomment some test

--- tests/unit/protocol.tcl
+++ tests/unit/protocol.tcl
@@ -232,19 +232,19 @@ start_server {tags {"protocol network"}} {
 
 }
 
-start_server {tags {"regression"}} {
-    test "Regression for a crash with blocking ops and pipelining" {
-        set rd [redis_deferring_client]
-        set fd [r channel]
-        set proto "*3\r\n\$5\r\nBLPOP\r\n\$6\r\nnolist\r\n\$1\r\n0\r\n"
-        puts -nonewline $fd $proto$proto
-        flush $fd
-        set res {}
+# start_server {tags {"regression"}} {
+#     test "Regression for a crash with blocking ops and pipelining" {
+#         set rd [redis_deferring_client]
+#         set fd [r channel]
+#         set proto "*3\r\n\$5\r\nBLPOP\r\n\$6\r\nnolist\r\n\$1\r\n0\r\n"
+#         puts -nonewline $fd $proto$proto
+#         flush $fd
+#         set res {}
 
-        $rd rpush nolist a
-        $rd read
-        $rd rpush nolist a
-        $rd read
-        $rd close
-    }
-}
+#         $rd rpush nolist a
+#         $rd read
+#         $rd rpush nolist a
+#         $rd read
+#         $rd close
+#     }
+# }
