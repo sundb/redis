75d43c75e43c2a34111b0aba7f74d63bc540baa1
debing.sun <debing.sun@redis.com>
2025-09-24 18:31:34 +0800

    uncomment tests

--- tests/support/test.tcl
+++ tests/support/test.tcl
@@ -162,7 +162,6 @@ proc search_pattern_list {value pattern_list {glob_pattern false}} {
 }
 
 proc test {name code {okpattern undefined} {tags {}}} {
-    puts $name
     # abort if test name in skiptests
     if {[search_pattern_list $name $::skiptests]} {
         incr ::num_skipped
