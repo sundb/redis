ad5758625cf5071b1e61041c4a00dbd4807f1507
debing.sun <debing.sun@redis.com>
2026-01-17 13:15:55 +0800

    test

--- src/lazyfree.c
+++ src/lazyfree.c
@@ -286,7 +286,7 @@ void emptyDbAsync(redisDb *db) {
     db->keys = kvstoreCreate(&kvstoreExType, &dbDictType, slot_count_bits, flags);
     db->expires = kvstoreCreate(&kvstoreBaseType, &dbExpiresDictType, slot_count_bits, flags);
     db->subexpires = estoreCreate(&subexpiresBucketsType, slot_count_bits);
-    protectClientReplyObjects(); /* Protect client reply objects before async free. */
+    // protectClientReplyObjects(); /* Protect client reply objects before async free. */
     emptyDbDataAsync(oldkeys, oldexpires, oldsubexpires);
 }
 
